7f0dd78ed3c56c6a9d0e67987a7eb61f3ee52ff4
MarkJur-afk <jurgenmarkk@gmail.com>
2025-09-22 09:22:10 +0300

    Add files via upload

---
+++ Tund1/inimene.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Jurgen
+{
+    public abstract class Inimene
+    {
+        
+        public string Nimi;
+        public int Vanus;
+        public Inimene()
+        {          
+        }
+
+        public Inimene(string nimi, int vanus) 
+        {
+            Nimi = nimi;
+            Vanus = vanus;
+        }
+
+        public void Tervita()
+        {
+            Console.WriteLine("Tere! Mina olen " + Nimi);
+        }
+        public abstract void Mida_Teeb();
+            
+
+        }
+    }
+
---
+++ ulesanne2/Guitar.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace ulesanne2;
+
+class Guitar : Instrument
+{
+    public Guitar(string nimi) : base(nimi) { }
+
+    public override string Play()
+    {
+        return "Mängib keelpille";
+    }
+}
+
